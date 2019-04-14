427b25c7e2c93a91e9119be9c345ecdea016aabe
Vishnu19 <vishnukp187@gmail.com>
2019-04-14 23:52:05 +0530

    Initial commit

---
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,44 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.List;
+
+//The annotation is a special type of @Component annotation which describes that the class defines a data repository
+@Repository
+public class CommentRepository {
+    //Get an instance of EntityManagerFactory from persistence unit with name as 'imageHoster'
+    @PersistenceUnit(unitName = "imageHoster")
+    private EntityManagerFactory emf;
+
+    //The method receives the User object to be persisted in the database
+    //Creates an instance of EntityManager
+    //Starts a transaction
+    //The transaction is committed if it is successful
+    //The transaction is rolled back in case of unsuccessful transaction
+    public void addComment(Comment newComment) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            //persist() method changes the state of the model object from transient state to persistence state
+            em.persist(newComment);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+    }
+
+    public List<Comment> getCommentbyTitle(Integer imageId) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<Comment> typedQuery = em.createQuery("SELECT i from Comment i where i.id =:id", Comment.class).setParameter("id", imageId);
+            return typedQuery.getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
